08ea855cbd04d04fa8cecfd5f05969430d6b8f53
zengjian <536500204@qq.com>
2020-10-30 17:33:22 +0800

    debug: 测试md

---
+++ readme.js
@@ -0,0 +1,33 @@
+const walk = require('walkdir')
+const fs = require('fs')
+const path = require('path')
+
+const emitter = walk('./src/icons/svg')
+const content = {
+  items: []
+}
+
+function parseCatalog(items) {
+  let str = ``
+  items.forEach(item => {
+    str += `![${item}](${item}) \n`
+  })
+  return str
+}
+
+emitter.on('file', function(file) {
+  if (file && file.endsWith('.svg')) {
+    const p = path.relative(__dirname, file)
+    content.items.push(p)
+  }
+})
+emitter.on('end', function() {
+  const mdCont = `
+#### svg图标搜集！enjoy！\n
+${parseCatalog(content.items)}
+  `
+
+  fs.writeFile('./README.md', mdCont, () => {
+    console.log('写入Readme完成')
+  })
+})
